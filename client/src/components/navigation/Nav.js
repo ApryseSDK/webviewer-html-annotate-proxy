001faee2a9e367b075c3e6c48097feba76af184c
maggievu <mvu@pdftron.com>
2022-01-21 15:24:52 -0800

    add http(s) prefix if there's none

--- client/src/components/navigation/Nav.js
+++ client/src/components/navigation/Nav.js
@@ -13,16 +13,29 @@ import './Nav.css';
 
 const Nav = ({ handleSubmit, fetchError, showSpinner, handleDownload, browseMode }) => {
   const [url, setUrl] = useState('');
+  const [urlWithHttp, setUrlWithHttp] = useState('');
   const [error, setError] = useState('');
+
+  const regexURL = /(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi;
+  const regexURLWithHttp = /^(http(s)?:\/\/.){1}/gi;
 
   useEffect(() => {
     setError(fetchError);
   }, [fetchError]);
 
+  useEffect(() => {
+    if (regexURLWithHttp.test(url)) {
+      setUrlWithHttp(url);
+    } else {
+      setUrlWithHttp(`https://${url}`);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [url]);
+
   // test is URL (without https://) is valid https://regexr.com/3e6m0
   const isValidURL = (url) => {
     // eslint-disable-next-line no-useless-escape
-    return /(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi.test(url);
+    return regexURL.test(url);
   }
 
   return (
@@ -51,7 +64,7 @@ const Nav = ({ handleSubmit, fetchError, showSpinner, handleDownload, browseMode
           disabled={showSpinner}
           onClick={() => {
             if (!!url && isValidURL(url)) {
-              handleSubmit(url);
+              handleSubmit(urlWithHttp);
             } else {
               setError('Please enter a valid URL and try again.');
             }
