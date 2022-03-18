616b1a9fa2787039708285677339dfd5ba91f4c4
maggievu <mvu@pdftron.com>
2022-03-18 11:13:25 -0700

    update options after adding webpack for server

--- server/index.js
+++ server/index.js
@@ -3,8 +3,8 @@ const HTMLProxyServer = require('@pdftron/webviewer-html-proxy-server');
 const OPTIONS = {
   SERVER_ROOT: `http://localhost`,
   PORT: 3100,
-  // CORS_OPTIONS: { origin: "http://localhost:3000", credentials: true },
-  // COOKIE_SETTING: { sameSite: 'none', secure: true },
+  CORS_OPTIONS: { origin: "http://localhost:3000", credentials: true },
+  COOKIE_SETTING: { sameSite: 'none', secure: true },
 };
 
 HTMLProxyServer.createServer(OPTIONS);
