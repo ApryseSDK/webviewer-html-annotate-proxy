5579115b15dc57f98a6e0f49ff264d96f9fe887b
maggievu <mvu@pdftron.com>
2022-05-10 14:10:32 -0700

    reuse env variables instead of hardcoding with localhost

--- server/index.js
+++ server/index.js
@@ -5,11 +5,12 @@ require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const PORT = process.env.PORT || 3000;
 const NODE_PORT = process.env.REACT_APP_NODE_PORT || 3100;
+const REACT_HOST = process.env.REACT_APP_HOST || 'localhost';
 
 const OPTIONS = {
-  SERVER_ROOT: `http://localhost`,
+  SERVER_ROOT: `http://${REACT_HOST}`,
   PORT: NODE_PORT,
-  CORS_OPTIONS: { origin: `http://localhost:${PORT}`, credentials: true },
+  CORS_OPTIONS: { origin: `http://${REACT_HOST}:${PORT}`, credentials: true },
   // COOKIE_SETTING: { sameSite: 'none', secure: true },
 };
 
